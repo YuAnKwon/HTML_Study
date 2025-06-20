59037d198ec1c3c57c5f488b2ec65c044cc717e1
YuAnKwon <dreamhunt17@gmail.com>
2025-06-20 09:11:31 +0900

    commit

--- mini_project/js/script.js
+++ mini_project/js/script.js
@@ -12,28 +12,41 @@ setInterval(clock,1000); //1초 주기로 실행
 
 // 프로필 사진 클릭시 번갈아 출력
 let toggle = 1;
+const introImg = document.getElementById("introImg");
 function changeImg(){
     if(++toggle % 2==0){
-        document.getElementById("introImg").src="img/white_puppy.jpg";
+        introImg.src="img/white_puppy.jpg";
     } else{
-        document.getElementById("introImg").src="img/shin_chan.jpg";
+        introImg.src="img/shin_chan.jpg";
     }
 }
 
-// 음식 사진시 scale
-let img = document.querySelectorAll(".recipe img");
-for(let i=0; i<imgs.length; i++){
-    img = addEventListener("click", function()=>)
+// 음식 사진 클릭시 scale
+const img = document.querySelectorAll(".recipe img");
+for(let i=0; i<img.length; i++){
+    img[i].addEventListener("click", function(){
+        let isScaled = img[i].style.transform == "scale(1.7)";
+        img[i].style.transform = isScaled ? "scale(1)" : "scale(1.7)";
+    });
+    
 }
 
 // contact 문자 회전
 const rotate = document.getElementById("rotate");
 function rotateString(){
     const text = rotate.textContent;
-    const lastChar = text[text.length -1];
-    const rotatedText = lastChar+ text.slice(0, text.length-1);
+    const firstChar = text[0];
+    const rotatedText = text.slice(1) + firstChar;
     rotate.textContent = rotatedText;
 };
 setInterval(rotateString, 500);
 
-//mousemove
+// 마우스 따라다니는 사진
+const image = document.getElementById("followMouse");
+document.onmousemove = updateImage;
+
+function updateImage(event){
+    const X = event.clientX;
+    const Y = event.clientY;
+    image.style.transform = `translate(${X}px, ${Y}px)`;
+}
